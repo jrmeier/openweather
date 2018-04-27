ae62891247bb0fb6959c84be102989e496e192ce
Jed Meier <jedmeier@jeds-mbp.misc.iastate.edu>
2018-04-27 00:29:27 -0500

    more cleaning

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from open_weather_api import OpenWeatherApi
-appid = ""
+appid = ''
 
 def check_weather():
     api = OpenWeatherApi(appid)
-    city_string = input("What city are you in? ")
+    city_string = input('What city are you in? ')
     print(api.get_weather_by_city(city_string))
 
 if __name__ == '__main__':
